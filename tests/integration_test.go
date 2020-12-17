539add2ff3c83c60fcba54380de6ce9a0fc38527
Alexander Kraynov <kraynovsasha@gmail.com>
2020-12-17 13:33:34 +0300

    Fixed comments

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -7,6 +7,18 @@ import (
 	"testing"
 )
 var base = "https://alfalfa-project.herokuapp.com/api"
+func authenticate() string {
+	var jsonStr = []byte(`{
+	"login": "abcdef",
+	"password": "abcdef"
+}`)
+	req, _ := http.NewRequest("POST", base + "/auth", bytes.NewBuffer(jsonStr))
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	token, _ := ioutil.ReadAll(resp.Body)
+	return string(token)
+}
 func TestLogin(t *testing.T) {
 	var jsonStr = []byte(`{
 	"login": "abcdef",
@@ -20,63 +32,37 @@ func TestLogin(t *testing.T) {
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		t.Errorf("register returned status %d, expected %d", resp.StatusCode, http.StatusOK)
+		t.Errorf("auth returned status %d, expected %d", resp.StatusCode, http.StatusOK)
 	}
 }
 
-func TestPostMeasurments(t *testing.T) {
-	var jsonStr = []byte(`{
-	"login": "abcdef",
-	"password": "abcdef"
-}`)
-	req, err := http.NewRequest("POST", base + "/auth", bytes.NewBuffer(jsonStr))
+func TestPostMeasurements(t *testing.T) {
 	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Errorf("Error posting request: %s", err	)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("register returned status %d, expected %d", resp.StatusCode, http.StatusOK)
-	}
-	token, _ := ioutil.ReadAll(resp.Body)
+	token := authenticate()
 	var jsonStr2 = []byte(`{   "temperature": 36.6,   "timestamp": "2012-04-23T18:25:43.511Z" }`)
-	req2, err2 := http.NewRequest("POST", base + "/measurements", bytes.NewBuffer(jsonStr2))
+	req2, err2 := http.NewRequest("POST", base + "/abcdef/measurements", bytes.NewBuffer(jsonStr2))
 	req2.Header.Set("Bearer", string(token))
-	resp2, err2 := client.Do(req)
+	resp2, err2 := client.Do(req2)
 	if err2 != nil {
-		t.Errorf("Error posting request: %s", err	)
+		t.Errorf("Error posting request: %s", err2	)
 	}
 	defer resp2.Body.Close()
 	if resp2.StatusCode != http.StatusOK {
-		t.Errorf("POST /measutments  returned status %d, expected %d", resp.StatusCode, http.StatusOK)
+		t.Errorf("POST /measurements  returned status %d, expected %d", resp2.StatusCode, http.StatusOK)
 	}
 }
 
-func TestGetMeasurments(t *testing.T) {
-	var jsonStr = []byte(`{
-	"login": "abcdef",
-	"password": "abcdef"
-}`)
-	req, err := http.NewRequest("POST", base + "/auth", bytes.NewBuffer(jsonStr))
+func TestGetMeasurements(t *testing.T) {
 	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Errorf("Error posting request: %s", err	)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("register returned status %d, expected %d", resp.StatusCode, http.StatusOK)
-	}
-	token, _ := ioutil.ReadAll(resp.Body)
-	req2, err2 := http.NewRequest("GET", base + "/measurements", nil)
+	token := authenticate()
+	req2, err2 := http.NewRequest("GET", base + "/abcdef/measurements", nil)
 	req2.Header.Set("Bearer", string(token))
-	resp2, err2 := client.Do(req)
+	resp2, err2 := client.Do(req2)
 	if err2 != nil {
-		t.Errorf("Error posting request: %s", err	)
+		t.Errorf("Error posting request: %s", err2	)
 	}
 	defer resp2.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("GET /measutments returned status %d, expected %d", resp.StatusCode, http.StatusOK)
+	if resp2.StatusCode != http.StatusOK {
+		t.Errorf("GET /measurements returned status %d, expected %d", resp2.StatusCode, http.StatusOK)
 	}
 }
